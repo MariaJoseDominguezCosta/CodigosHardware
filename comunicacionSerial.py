6f8f8951eb4c324011b856de730dc03319608b4c
María José <93809146+MariaJoseDominguezCosta@users.noreply.github.com>
2023-07-22 18:46:51 -0600

    Update comunicacionSerial.py

--- comunicacionSerial.py
+++ comunicacionSerial.py
@@ -13,14 +13,11 @@ try:
         # ~ )
     # ~ cursor = conn.cursor()
     arduino = serial.Serial('/dev/ttyUSB0', 9600)  # Asegúrate de ajustar el puerto correcto
-    # ~ user_id=requests.get('http://backend-growtech.servehttp.com/login')
     token=""
     
-    response= requests.post('http://backend-growtech.servehttp.com/login/', data={"username": "maria", "password":"1234"})
+    response= requests.post('http://growtech.ddns.net/login/', data={"username": "maria", "password":"1234"})
     token = response.json()['token']
-    user_id= response.json()['user_id']
-    response2 = requests.get('http://backend-growtech.servehttp.com/plants/', data={"usuario_id":user_id})
-    print(response2)
+    print(token)
     while True:
         line = arduino.readline().decode().strip()
         data = line.split(':')
@@ -28,7 +25,7 @@ try:
         if data[0] == 'TEMP':
             temperature = float(data[1])
             # Guardar temperatura en la base de datos Django
-            response= requests.post('http://backend-growtech.servehttp.com/temperatura/',
+            response= requests.post('http://growtech.ddns.net/temperatura/',
              data={'valor': temperature},
              headers={"Authorization": "Bearer "+ token})
             print(response.content)
@@ -36,21 +33,22 @@ try:
         elif data[0] == 'HUM':
             humidity = float(data[1])
             # Guardar humedad en la base de datos Django
-            requests.post('http://backend-growtech.servehttp.com/humedad/ambiente/', data={'valor': humidity,
-             headers={"Authorization": "Bearer "+ token})
+            response = requests.post('http://growtech.ddns.net/humedad/ambiente/', 
+            data={'valor': humidity},
+            headers={"Authorization": "Bearer "+ token})
             print(response.content)
 
         elif data[0] == 'HUM_SUELO ':
             soil_moisture = float(data[1])
             # Guardar humedad del suelo en la base de datos Django
-            requests.post('http://backend-growtech.servehttp.com/humedad/suelo/', data={'valor': soil_moisture,
+            response = requests.post('http://growtech.ddns.net/humedad/suelo/', data={'valor': soil_moisture},
              headers={"Authorization": "Bearer "+ token})
             print(response.content)
 
         elif data[0] == 'UV_INTENSITY':
             uv_intensity = float(data[1])
             # Guardar intensidad de luz UV en la base de datos Django
-            requests.post('http://backend-growtech.servehttp.com/intensidad/', data={'valor': uv_intensity,
+            response = requests.post('http://growtech.ddns.net/intensidad/', data={'valor': uv_intensity},
              headers={"Authorization": "Bearer "+ token})
             print(response.content)
 
